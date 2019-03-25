664bcbe7736188df3feae283b763f4bf817c68bc
Stavros <sst46@nau.edu>
2019-03-24 21:22:28 -0700

    added log dump

--- ConfigUploadDir/PA02_873543/Sim2Combined/ProcessCtrl.c
+++ ConfigUploadDir/PA02_873543/Sim2Combined/ProcessCtrl.c
@@ -2,7 +2,8 @@
 #include "MetaDataAccess.h"
 #include "ProcessCtrl.h"
 #include "simtimer.h"
-#include <pthread.h>
+#include "threadManager.h"
+//#include <pthread.h>
 
 
 
@@ -34,8 +35,26 @@ void simRun(  ConfigDataType *configPtr, OpCodeType *currentPtr )
 	char *timeStr = (char *) malloc(MAX_STR_LENGTH);
 	char *messageStr = (char *) malloc(MAX_STR_LENGTH);
 
+	Boolean endOfProgram = False;
+
+
 	ProgRun *pcb;
 	int timeRemaining;
+	OpCodeType *tempPtr;
+   pthread_t thread0;
+	
+	Boolean endOfProgram = False;
+   int size, timeLeft, tempInt;
+   OpCode *tempPtr;
+   ProcessControlBlock *pcb;
+   char *fileName = (char *) malloc(MAX_STR_LENGTH);
+   char *timeStr = (char *) malloc(MAX_STR_LENGTH);
+   char *displayString = (char *) malloc(MAX_STR_LENGTH);
+   char *messageStr = (char *) malloc(MAX_STR_LENGTH);
+   char *tempStr = (char *) malloc(MAX_STR_LENGTH);
+   pthread_t thread0;
+
+
 
 
 
@@ -102,95 +121,238 @@ void simRun(  ConfigDataType *configPtr, OpCodeType *currentPtr )
             pcb->currentIndex );
    pcb->currentIndex++;
 
+	 //start program
+
+   // we are using tempPtr to progress through the list
+   //Set processes to Running state
+   displayStr = "   ";
+   accessTimer( LAP_TIMER, &timeStr ); 
+   messageStr = ", OS: Process 0 set in RUNNING state\n\n";
+   createDisplayStr ( &displayStr, timeStr, messageStr );
+   display( pcb->monitorFlag, pcb->fileFlag, &displayString , pcb->fileBuffer, 
+            pcb->currentIndex );
+   pcb->currentIndex++;
+
+   //move temp pointer to first operation
+   tempPtr = tempPtr->next
+   while( endOfProgram != True )
+   {
+     // create thread
+     pthread_create( &thread0, NULL, threadEntry, &pcb);
+     // wait for thread to complete
+     pthread_join( thread0, NULL );
+     // move pointer to next program
+     tempPtr = tempPtr->next;
+     // The only 'S' found in the loop is S( end )
+     if( tempPtr->opLtr == 'S' )
+       {
+          endOfProgram = True;
+       }
+   }  
+
+	// display end of flag
+   displayStr = "\n"; 
+   display( pcb->monitorFlag, pcb->fileFlag, &displayString , pcb->fileBuffer, 
+            pcb->currentIndex );
+   pcb->currentIndex++;
+   
+   //end program
+   displayStr = "   ";
+   accessTimer( LAP_TIMER, &timeStr ); 
+   messageStr = ", OS: Process 0 ended and set in EXIT state\n\n";
+   createDisplayStr ( &displayStr, timeStr, messageStr );
+   display( pcb->monitorFlag, pcb->fileFlag, &displayString , pcb->fileBuffer, 
+            pcb->currentIndex );
+   pcb->currentIndex++;
+
+   //stop system
+   displayStr = "   ";
+   accessTimer( STOP_TIMER, &timeStr ); 
+   messageStr = ", OS: System Stop\n\n";
+   createDisplayStr ( &displayStr, timeStr, messageStr );
+   display( pcb->monitorFlag, pcb->fileFlag, &displayString , pcb->fileBuffer, 
+            pcb->currentIndex );
+   pcb->currentIndex++;
+
+	// print the final monitor output
+   if( pcb->monitorFlag )
+   {
+      printf("End Simulation - Complete\n");
+      printf("=========================\n\n");
+   }
+   // dump log to file
+   logDump( pcb );
+   // free memory
+   clearUploadData
+   free( displayString );
+   free( timeString );
+   //free( fileName );
+   free( messageStr );
+   free( tempStr );
+}
 
 
+//function to print all required output to the log file
+void logDump( ProgRun *pcb);
+{
+   char *tempStr = (char *) malloc(MAX_STR_LENGTH);
+   char *anotherTempStr = (char *) malloc(MAX_STR_LENGTH);
+   char *fileName = (char *) malloc(MAX_STR_LENGTH);
+   ConfigDataType *tempConfigPtr = pcb->accessPtr;
+   int index = 0;
 
+   // open file 'w' clears the file for a new dump
+   fileName = tempConfigPtr->logToFileName;
+   File *filePtr = freopen( fileName, 'w' );
 
+   // begin file write
+   fprintf(filePtr, "==================================================\n");
+   fprintf(filePtr, "Simulator Log File Header\n\n");
 
-	printf( "\nSystem Start");
-	//access at 0 timer
-	accessTimer( ZERO_TIMER, timeString );
-	//possibly a timeToString call
-	//void timeToString( int secTime, int uSecTime, char *timeStr )
-	//timeToString( , , timeString );
+   // concatenate meta filename and newline
+   // reusing filename so less memory is used, the pointer is all I need
+   fileName = tempConfigPtr->metaDataFileName;
+   tempStr = "File Name                       : ";
+   concatenateString( tempStr, fileName );
+   concatenateString( tempStr, "\n" );
+   fprintf( filePtr, "%s", tempStr );
 
-	printf( "\nCreating Process Control Blocks");
-	//call function to create PCB
-	//PCB_LL *LinkedList = createPCB( configPtr, currentPtr ); //function needs to create PCBS
-	createPCB( configPtr, currentPtr );
-	//access timer LAP
-	accessTimer( LAP_TIMER, timeString );
+   // scheduling doesn't change
+   fprintf(filePtr, "CPU Scheduling                  : FCFS-N\n");
 
-	
+   // print quant cycles
+   tempStr ="Quantum Cycles                  : ";
+   sprintf( anotherTempStr, "%d", tempConfigPtr->quantumCycles);
+   concatenateString( tempStr, anotherTempStr );
+   concatenateString( tempStr, "\n" );
+   fprintf( filePtr, "%s", tempStr );
 
-//all processes initialized in new state
+   // print mem available
+   tempStr ="Memory Available (KB)           : ");
+   sprintf( anotherTempStr, "%d", tempConfigPtr->memAvailable);
+   concatenateString( tempStr, anotherTempStr );
+   concatenateString( tempStr, "\n" );
+   fprintf( filePtr, "%s", tempStr );
 
-	//for loop setting new process states to NEW
-	while( currentPtr != NULL )
-		{
-			//ProcessControl->
-			currentPtr = currentPtr->next;
-		}
+   // print processor cycle rate
+   tempStr = "Processor Cycle Rate (ms/cycle) : ");
+   sprintf( anotherTempStr, "%d", tempConfigPtr->procCycleRate);
+   concatenateString( tempStr, anotherTempStr );
+   concatenateString( tempStr, "\n" );
+   fprintf( filePtr, "%s", tempStr );
 
-	printf( "\nAll processes initilized in " );
-	//printf( "%s", LinkedList->stateOfProcess );
-	printf( " state");
+   // print I/O cycle rate
+   tempStr ="I/O Cycle Rate (ms/cycle)       : ");
+   sprintf( anotherTempStr, "%d", tempConfigPtr->ioCycleRate);
+   concatenateString( tempStr, anotherTempStr );
+   concatenateString( tempStr, "\n" );
+   fprintf( filePtr, "%s", tempStr );
 
-
-	//ACCESS TIMER LAP
-	accessTimer( LAP_TIMER, timeString );
-
-//all processes initialized in ready state
-
-	//run loop to set in READY state
-	while( currentPtr != NULL )
-		{
-		//	LinkedList->stateOfProcess = READY;
-			currentPtr = currentPtr->next;
-		}
-
-	printf( "\nAll processes now set in " );
-	//printf( "%s", LinkedList->stateOfProcess );
-	printf( " state");
-
-	//ACCESS LAP TIMER
-	accessTimer( LAP_TIMER, timeString );
-
-	//"Process" + PID + "se;ected with" + RemainingTime + "ms remaining"
-	printf( "\n Process " );
-//	printf( "%s", LinkedList->PID );
-	printf( " selected with ");
-//	printf( "%d" , LinkedList->timeRemaining );
-	printf( " ms remaining\n" );
-
-	//ACCESS LAP TIMER
-	accessTimer( LAP_TIMER, timeString );
-
-
-	//set Process 0 to running state
-	//LinkedList->stateOfProcess = RUNNING;
-
-	//"Process" + PID + "set in " + stateOfOperation + "state"
-	printf( "\n Process " );
-//	printf( "%s", LinkedList->PID );
-	printf( " set in ");
-//	printf( "%d" , LinkedList->stateOfProcess );
-	printf( " state" );
-
-	//ACCESS LAP TIMER
-	accessTimer( LAP_TIMER, timeString );
-
-
-
-	//loop
+   //print the excess
+   fprintf("================\n");
+   fprintf("Begin Simulation\n\n");
+   
+   //loop through buffer and print it
+   while( index < pcb->currentIndex )
+   {
+      tempStr = pcb->fileBuffer[index];
+      fprintf( filePtr, "%s", tempStr );
+      index++;
+   }
+   
+   //print end bit
+   fprintf(filePtr, "End Simulation - Complete\n");
+   fprintf(filePtr, "=========================\n\n");
+   fclose( filePtr );
+   //free data
+   free( tempStr );
+   free(anotherTempStr);
+   free( fileName );
 
 }
 
-void *runner( void *param )
-	{
-		//calcRemainingTime( )
 
-	}
+
+
+
+// 	printf( "\nSystem Start");
+// 	//access at 0 timer
+// 	accessTimer( ZERO_TIMER, timeString );
+// 	//possibly a timeToString call
+// 	//void timeToString( int secTime, int uSecTime, char *timeStr )
+// 	//timeToString( , , timeString );
+
+// 	printf( "\nCreating Process Control Blocks");
+// 	//call function to create PCB
+// 	//PCB_LL *LinkedList = createPCB( configPtr, currentPtr ); //function needs to create PCBS
+// 	createPCB( configPtr, currentPtr );
+// 	//access timer LAP
+// 	accessTimer( LAP_TIMER, timeString );
+
+	
+
+// //all processes initialized in new state
+
+// 	//for loop setting new process states to NEW
+// 	while( currentPtr != NULL )
+// 		{
+// 			//ProcessControl->
+// 			currentPtr = currentPtr->next;
+// 		}
+
+// 	printf( "\nAll processes initilized in " );
+// 	//printf( "%s", LinkedList->stateOfProcess );
+// 	printf( " state");
+
+
+// 	//ACCESS TIMER LAP
+// 	accessTimer( LAP_TIMER, timeString );
+
+// //all processes initialized in ready state
+
+// 	//run loop to set in READY state
+// 	while( currentPtr != NULL )
+// 		{
+// 		//	LinkedList->stateOfProcess = READY;
+// 			currentPtr = currentPtr->next;
+// 		}
+
+// 	printf( "\nAll processes now set in " );
+// 	//printf( "%s", LinkedList->stateOfProcess );
+// 	printf( " state");
+
+// 	//ACCESS LAP TIMER
+// 	accessTimer( LAP_TIMER, timeString );
+
+// 	//"Process" + PID + "se;ected with" + RemainingTime + "ms remaining"
+// 	printf( "\n Process " );
+// //	printf( "%s", LinkedList->PID );
+// 	printf( " selected with ");
+// //	printf( "%d" , LinkedList->timeRemaining );
+// 	printf( " ms remaining\n" );
+
+// 	//ACCESS LAP TIMER
+// 	accessTimer( LAP_TIMER, timeString );
+
+
+// 	//set Process 0 to running state
+// 	//LinkedList->stateOfProcess = RUNNING;
+
+// 	//"Process" + PID + "set in " + stateOfOperation + "state"
+// 	printf( "\n Process " );
+// //	printf( "%s", LinkedList->PID );
+// 	printf( " set in ");
+// //	printf( "%d" , LinkedList->stateOfProcess );
+// 	printf( " state" );
+
+// 	//ACCESS LAP TIMER
+// 	accessTimer( LAP_TIMER, timeString );
+
+
+
+// 	//loop
+
+// }
 
 
 
